9ac51d8b30c588dd28a983deed96f6aefcb161b3
Chris Daly <chris.daly@jisc.ac.uk>
2025-03-26 13:29:13 +0000

    Fixed missing parameter in plugin class

--- ckanext/matomo_js/plugin.py
+++ ckanext/matomo_js/plugin.py
@@ -10,6 +10,6 @@ import ckan.plugins.toolkit as toolkit
 class UKDSMatomoJSPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
 
-    def update_config(config):
+    def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_resource('assets', 'matomo_webassets')
